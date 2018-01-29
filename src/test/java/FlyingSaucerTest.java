7ce06758162667b1315ace0579c6d4bcb58746ff
Thomas Uhrig <thomas.uhrig@westernacher.com>
2018-01-29 10:02:07 +0100

    Initial commit

---
+++ src/test/java/FlyingSaucerTest.java
@@ -0,0 +1,103 @@
+import com.itextpdf.text.pdf.BaseFont;
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.w3c.tidy.Tidy;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import static org.thymeleaf.templatemode.TemplateMode.HTML;
+
+/**
+ * This is a JUnit test which will generate a PDF using Flying Saucer
+ * and Thymeleaf templates. The PDF will display a letter styled with
+ * CSS. The letter has two pages and will contain text and images.
+ *
+ * Simply run this test to generate the PDF. The file is called:
+ *
+ *      /test.pdf
+ */
+public class FlyingSaucerTest {
+
+    private static final String OUTPUT_FILE = "test.pdf";
+
+    @Test
+    public void generatePdf() throws Exception {
+
+        // We set-up a Thymeleaf rendering engine. All Thymeleaf templates
+        // are HTML-based files located under "src/test/resources". Beside
+        // of the main HTML file, we also have partials like a footer or
+        // a header. We can re-use those partials in different documents.
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(HTML);
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        // The data in our Thymeleaf templates is not hard-coded. Instead,
+        // we use placeholders in our templates. We fill these placeholders
+        // with actual data by passing in an object. In this example, we will
+        // write a letter to "John Doe".
+        //
+        // Note that we could also read this data from a JSON file, a database
+        // a web service or whatever.
+        Data data = exampleDataForJohnDoe();
+
+        Context context = new Context();
+        context.setVariable("data", data);
+
+        String htmlContent = templateEngine.process("template", context);
+        htmlContent = cleanData(htmlContent);
+
+        OutputStream os = new FileOutputStream(OUTPUT_FILE);
+        ITextRenderer renderer = new ITextRenderer();
+
+        renderer.getFontResolver().addFont("Code39.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+        renderer.setDocumentFromString(htmlContent);
+        renderer.layout();
+        renderer.createPDF(os);
+
+        os.close();
+    }
+
+    private Data exampleDataForJohnDoe() {
+        Data data = new Data();
+        data.setFirstname("John");
+        data.setLastname("Doe");
+        data.setStreet("Example Street 1");
+        data.setZipCode("12345");
+        data.setCity("Example City");
+        return data;
+    }
+
+    @lombok.Data
+    static class Data {
+        private String firstname;
+        private String lastname;
+        private String street;
+        private String zipCode;
+        private String city;
+    }
+
+    private String cleanData(String data) throws UnsupportedEncodingException {
+        Tidy tidy = new Tidy();
+        tidy.setInputEncoding("UTF-8");
+        tidy.setOutputEncoding("UTF-8");
+        tidy.setXHTML(true);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(data.getBytes("UTF-8"));
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        tidy.parseDOM(inputStream, outputStream);
+        return outputStream.toString("UTF-8");
+    }
+
+}
